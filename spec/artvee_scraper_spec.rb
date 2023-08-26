82f1dbd4c12d2e6d8499fa68721cd17349b8cf26
Leon Siqueira <leon.siqueir4@gmail.com>
2023-08-25 21:44:44 -0300

    spec: ArtveeScraper module tests

--- spec/artvee_scraper_spec.rb
+++ spec/artvee_scraper_spec.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 require 'net/http'
 require 'uri'
 require 'webmock/rspec'
@@ -6,7 +5,6 @@ require 'nokogiri'
 require_relative '../lib/artvee_scraper'
 
 RSpec.describe ArtveeScraper do
-  let(:resource) { described_class }
   let(:valid_url) { 'https://artvee.com/' }
 
   let(:contents) do
@@ -32,38 +30,46 @@ RSpec.describe ArtveeScraper do
     ]
   end
 
-  context 'when the HTTP request is successful (status code 200)' do
-    let(:body) do
-      cards = contents.map do |content|
-        "<div class='product-grid-item product woodmart-hover-tiled'><div>
-        <img src='#{content[:src]}'/>
-        <h3>#{content[:title]} (#{content[:date]}) </h3>
-        <div class='woodmart-product-brands-links'><a>#{content[:artist_name]}</a> (#{content[:details]})</div>
-        <div class='woodmart-product-cats'><a>#{content[:tag]}</a></div>
-        </div></div>"
-      end.join
+  let(:html_body) do
+    cards = contents.map do |content|
+      "<div class='product-grid-item product woodmart-hover-tiled'><div>
+      <img src='#{content[:src]}'/>
+      <h3>#{content[:title]} (#{content[:date]}) </h3>
+      <div class='woodmart-product-brands-links'><a>#{content[:artist_name]}</a> (#{content[:details]})</div>
+      <div class='woodmart-product-cats'><a>#{content[:tag]}</a></div>
+      </div></div>"
+    end.join
 
-      "<html><body>#{cards}</body></html>"
-    end
-    before do
-      allow(HttpFetcher).to receive(:call).with(valid_url).and_return(body)
-    end
-
-    it 'returns the response body' do
-      result = resource.scrape
-      expect(result.class).to eq(Array)
-    end
+    "<html><body>#{cards}</body></html>"
   end
 
   context 'when the HTTP request fails' do
-    let(:body) { nil }
     before do
-      allow(HttpFetcher).to receive(:call).with(valid_url).and_return(nil)
+      allow(HttpFetcher).to receive(:call)
     end
 
     it 'returns nil' do
-      result = resource.scrape
-      expect(described_class.scrape).to eq([])
+      expect(ArtveeScraper.scrape).to eq([])
+    end
+  end
+
+  context 'when the HTTP request is successful (status code 200)' do
+    it 'returns the response body' do
+      allow(HttpFetcher).to receive(:call).and_return(html_body)
+      result ||= ArtveeScraper.scrape
+      expect(result.count).to eq(3)
+      expect(result.first).to eq({
+                                   img_url: 'https://mdl.artvee.com/sftb/storageid1.jpg',
+                                   title: 'Art title 1',
+                                   date: '1945',
+                                   artist: 'Artist name 1',
+                                   artist_details: {
+                                     birth_date: '1905',
+                                     passing_date: '1970',
+                                     nationality: 'Spanish'
+                                   },
+                                   tag: 'Tag 1'
+                                 })
     end
   end
 end
